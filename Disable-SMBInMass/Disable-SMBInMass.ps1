8ba11a17faa02185502cb7d271b158c9787b98d0
jeremymcgee73 <jeremymcgee6+git@gmail.com>
2017-04-17 14:27:07 -0600

    You must reboot!

--- Disable-SMBInMass/Disable-SMBInMass.ps1
+++ Disable-SMBInMass/Disable-SMBInMass.ps1
@@ -1,4 +1,5 @@
 ﻿#This is a quick script I wrote to disable SMBv1 on Server 2008, and remove it on Server 2012
+#You must reboot after!
 Get-ADComputer -filter * -SearchBase "OU=Systems,DC=your,DC=domain" | ForEach-Object {
     $HostName = $_.DNSHostName
     $OSversion = $(Get-WmiObject -Computer $HostName -Class Win32_OperatingSystem).caption
