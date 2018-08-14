5d2b0e266ad2d79131e29132d1dcdf5c79f9625c
Jeremy McGee <JMcGee@younglife.org>
2018-08-14 14:24:10 -0600

    Example test for logging error

---
+++ Test-Logging/CustomTarget/Test.ps1
@@ -0,0 +1,19 @@
+﻿@{
+    Name = 'Test'
+    Configuration = @{
+        Level       = @{Required = $false;  Type = [string]}
+        Format      = @{Required = $false;  Type = [string]}
+        Owner       = @{Required = $true;  Type = [string]}
+        Path        = @{Required = $true;  Type = [string]}
+        Environment = @{Required = $true;  Type = [string]}
+    }
+    Logger = {
+        param(
+            $Log,
+            $Format,
+            [hashtable] $Configuration
+        )
+
+        Write-Output "test"
+    }
+}
---
+++ Test-Logging/test.ps1
@@ -0,0 +1,28 @@
+﻿$Environment = "Production"
+$Owner = "Testers"
+
+$ErrorActionPreference = "Stop"
+
+Import-Module Logging
+
+Try {
+    Set-LoggingCustomTarget -Path C:\scripts\Test-Logging\CustomTarget -Verbose
+    Set-LoggingDefaultLevel -Level 'INFO'
+    #Get-LoggingTargetAvailable
+
+            
+    Add-LoggingTarget -Name Console -Configuration @{}
+    Add-LoggingTarget -Name Test -Configuration @{
+            Owner = $Owner
+            Path = $Path
+            Environment = $Environment
+    }
+    if(!(Get-LoggingTargetAvailable).ContainsKey("Test")){
+        "Test target does not exist"
+    }
+}Catch{
+    "Error!"
+    $_
+}
+
+#Test running this script by running '1 .. 100 |% { powershell -file "C:\scripts\Test-Logging\test.ps1"}' in the PS console.
